d8f732dcf614a9e16847c466a8d53f8393b169fc
Jesus Delmoral <jdelmoral@students.mchenry.edu>
2020-09-11 18:51:40 -0500

    new

--- src/numb.kt
+++ src/numb.kt
@@ -3,8 +3,7 @@ import java.text.DecimalFormat
 fun main (args: Array<String>){
 //    val tax = .07
 //    val cost = 100
-//
-    val percent = DecimalFormat("00.00%")
+// formatting
     val money = DecimalFormat("$###,###.00")
 
 //
@@ -13,9 +12,10 @@ fun main (args: Array<String>){
 //
 //
 //     println("Result: $ourTestVar")
+//empty variables
     var total = 0
     var ans = 0
-
+// loop until 0 is entered
     do {
         println("Please enter the price of items, enter 0 to stop: ")
         print("Enter Your Choice: ")
@@ -23,15 +23,16 @@ fun main (args: Array<String>){
         total += ans
 
     }while (ans != 0)
-
+// processing
     var taxes = total *.025
     var tip = total * .175
     var realTotal = total + taxes + tip
-
+//    formatting
     var formatTotal = money.format(total)
     var formatTaxes = money.format(taxes)
     var formatTip = money.format(tip)
     var formatReal = money.format(realTotal)
+//    final print
     println("Subtotal: $formatTotal\nTaxes: $formatTaxes\n" +
             "Tip: $formatTip\nTotal: $formatReal")
 
